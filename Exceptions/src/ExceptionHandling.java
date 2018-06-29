90a483fadccc5c6e6bbb235c7d9278dade6dbbbd
Quan <urjhams@gmail.com>
2018-06-29 16:32:46 +0200

    update

--- Exceptions/src/ExceptionHandling.java
+++ Exceptions/src/ExceptionHandling.java
@@ -1,4 +1,3 @@
-
 public class ExceptionHandling {
 	public static void main(String[] args) {
 		//todo:
--- Exceptions/src/NegativeArgumentException.java
+++ Exceptions/src/NegativeArgumentException.java
@@ -1,3 +1,8 @@
 public class NegativeArgumentException extends Exception {
-  
+  public NegativeArgumentException() {
+    super();
+  }
+  public NegativeArgumentException(String message) {
+    super(message);
+  }
 }
